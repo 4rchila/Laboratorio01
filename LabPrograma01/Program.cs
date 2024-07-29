16249c50ce8d1033f8c5a6f4a6449526651ab9f2
German Archila <106097211+4rchila@users.noreply.github.com>
2024-07-29 09:30:53 -0600

    Cambios definitvos

--- LabPrograma01/Program.cs
+++ LabPrograma01/Program.cs
@@ -1,31 +1,51 @@
-﻿    List<string> nombreProductos = new List<string>();
-    int sumaPrecios = 0;
-    Console.WriteLine("Ingresa el la cantidad de productos que deseas llevar");
-    var cantidadProductos = int.Parse(Console.ReadLine());
-
-
-    for (int i = 0; i < cantidadProductos; i++)
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        try
+        List<string> nombreProductos = new List<string>();
+        int sumaPrecios = 0;
+        int Cantidad = 0;
+        Console.WriteLine("Ingresa el la cantidad de productos que deseas llevar");
+        var cantidadProductos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < cantidadProductos; i++)
         {
-            Console.WriteLine($"Ingresa el nombre del producto No:{i}");
-            nombreProductos.Add(Console.ReadLine());
+            try
+            {
+                Console.WriteLine($"Ingresa el nombre del producto No:{i + 1}");
+                nombreProductos.Add(Console.ReadLine());
 
-            Console.WriteLine($"Escribe el precio del producto que Ingresaste");
-            sumaPrecios = sumaPrecios + int.Parse(Console.ReadLine());
+                Console.WriteLine($"Escribe el precio del producto que Ingresaste");
+                sumaPrecios = sumaPrecios + int.Parse(Console.ReadLine());
 
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message + "Error de formato");
+            }
         }
-        catch (FormatException ex)
+
+        static int aplicarDescuento(int Cantidad, int sumaPrecios)
         {
-            Console.WriteLine(ex.Message + "Error de formato");
+            Cantidad = (int)(sumaPrecios - (sumaPrecios * 0.40));
+            return Cantidad;
+        }
+        if (sumaPrecios > 5000)
+        {
+            Console.Clear();
+            Console.WriteLine("Sobre pasaste los Q5000.00, por ello se aplicara un descuento del 40% en el precio total de los productos");
+            Console.WriteLine("El precio total a pagar es de:");
+            Console.WriteLine(aplicarDescuento(Cantidad, sumaPrecios));
+        }
+        else 
+        {
+            Console.Clear();
+            foreach (var p in nombreProductos) 
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine($"La cantidad a pagar por los productos es: {sumaPrecios}");
+
         }
     }
-
- static int aplicarDescuento() 
-{ 
-
-}
-if () 
-{
-    aplicarDescuento();
 }
