2521fe9baa0c6f34b4dfe360d0832dae8dae9905
German Archila <106097211+4rchila@users.noreply.github.com>
2024-07-29 08:59:45 -0600

    Agregar archivos de proyecto.

---
+++ LabPrograma01/Program.cs
@@ -0,0 +1,31 @@
+﻿    List<string> nombreProductos = new List<string>();
+    int sumaPrecios = 0;
+    Console.WriteLine("Ingresa el la cantidad de productos que deseas llevar");
+    var cantidadProductos = int.Parse(Console.ReadLine());
+
+
+    for (int i = 0; i < cantidadProductos; i++)
+    {
+        try
+        {
+            Console.WriteLine($"Ingresa el nombre del producto No:{i}");
+            nombreProductos.Add(Console.ReadLine());
+
+            Console.WriteLine($"Escribe el precio del producto que Ingresaste");
+            sumaPrecios = sumaPrecios + int.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message + "Error de formato");
+        }
+    }
+
+ static int aplicarDescuento() 
+{ 
+
+}
+if () 
+{
+    aplicarDescuento();
+}
